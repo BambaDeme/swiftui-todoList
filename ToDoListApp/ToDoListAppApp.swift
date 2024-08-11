36b28bd92ce2cb171910b3d86cc52360c3450317
bambademe <cheikhbambademe@gmail.com>
2024-08-11 16:39:56 +0000

    Initial Commit

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by cheikh ahmadou bamba deme  on 11/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
